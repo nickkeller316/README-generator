d4884cb744cffb79d8787955bd575193c7a8807b
Nick Keller <nike0821@colorado.edu>
2020-12-23 18:23:43 -0800

    Final commit

--- Develop/index.js
+++ Develop/index.js
@@ -79,7 +79,7 @@ function writeToFile(fileName, data) {
     });
 }
 
-//initializing the function
+//initializing the program
 function init() {
     inquirer.prompt(questions).then((response) => {
         let data = { ...response };
